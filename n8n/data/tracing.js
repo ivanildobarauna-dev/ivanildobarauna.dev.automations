41adc6b00e474edae7df58ccd00b7aaa3d197e6e
IvanildoBarauna <ivanildo.jnr@oulook.com>
2025-05-29 12:56:17 -0300

    hotfix: node path

--- n8n/data/tracing.js
+++ n8n/data/tracing.js
@@ -1,11 +1,11 @@
-const tracer = require('dd-trace').init({
-    service: 'n8n',
-    hostname: process.env.DD_AGENT_HOST || 'localhost',
-    env: process.env.DD_ENV || 'development',
-    version: process.env.DD_VERSION || 'local',
-    logInjection: true,
-    runtimeMetrics: true,
-    analytics: true
-  });
+// const tracer = require('dd-trace').init({
+//     service: 'n8n',
+//     hostname: process.env.DD_AGENT_HOST || 'localhost',
+//     env: process.env.DD_ENV || 'development',
+//     version: process.env.DD_VERSION || 'local',
+//     logInjection: true,
+//     runtimeMetrics: true,
+//     analytics: true
+//   });
 
-console.log('[dd-trace] Tracing initialized ✅');
+// console.log('[dd-trace] Tracing initialized ✅');
